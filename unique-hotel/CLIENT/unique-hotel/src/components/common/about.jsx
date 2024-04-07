3322642967d7280a1e7318f0e0c03348a309c3c7
Sona0202 <130206732+Sona0202@users.noreply.github.com>
2024-04-07 11:25:54 +0530

    Add files via upload

---
+++ unique-hotel/CLIENT/unique-hotel/src/components/common/about.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Container } from "react-bootstrap"
+const about = () => {
+  return (
+  
+		<><div id='about'>
+            <br /><br />
+          <Container className="text-center px-5 py-5 justify-content-center parallax mb-5">
+              <div className="animated-texts bounceIn">
+                  <h1>
+                      ABOUT US
+                  </h1>
+                  <h3>We offer the best services for all your needs.</h3>
+              </div>
+          </Container>
+
+          <div class="card" >
+  {/* <img src="..." class="card-img-top" alt="..."> */}
+  <div class="card-body">
+    <p class="card-text"><h1><b>Find Your Perfect Accommodation </b> </h1>
+Finding the perfect accommodation in the  our hotel can be a daunting task, but Agoda.com makes it easy. With over 1 million hotels, apartments, and vacation rentals, Agoda.com has something for every traveler and budget. Whether you're looking for a luxury hotel in New York City or a budget-friendly hostel in Los Angeles, Agoda.com has you covered. With detailed descriptions, photos, and reviews from other travelers, you can find the perfect accommodation that meets your needs. Agoda.com also offers 24/7 customer service, so you can rest assured that you'll have support throughout your trip. Plus, with Agoda.com's Best Price Guarantee, you can be confident that you're getting the best deal on your accommodation.</p>
+    <p>
+   <h1><b> What to Eat in the Our Hotel</b></h1>
+The our hotel has a diverse food scene, with each region offering its unique flavors and dishes. Here are some must-try foods in the United States: - New York-style pizza: Thin crust, tomato sauce, and gooey cheese make New York-style pizza a favorite across the country. - Southern barbecue: Slow-cooked meats with tangy barbecue sauce are a staple in the southern states. - Tex-Mex cuisine: A fusion of Mexican and Texan flavors, Tex-Mex cuisine includes dishes like tacos, fajitas, and chili con carne. - Lobster rolls: A New England classic, lobster rolls are a sandwich filled with buttery lobster meat. - Chicago-style hot dogs: A hot dog on a poppy seed bun topped with mustard, relish, onions, pickles, and peppers is a Chicago favorite. - Philly cheesesteak: Thinly sliced steak, melted cheese, and grilled onions on a hoagie roll make the Philly cheesesteak a must-try in Philadelphia.
+    </p>
+  </div>
+</div>
+
+      </div></>
+
+	)
+}
+
+export default about
---
+++ unique-hotel/CLIENT/unique-hotel/src/components/common/contact.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import { Element } from "react-scroll";
+const contact = () => {
+    return (
+  <>
+
+		
+		<div >
+		<Element id="contact" name='contact'>
+			<br /><br />
+			<Container className="text-center px-5 py-5 justify-content-center parallax mb-5" >
+				<div className="animated-texts bounceIn">
+					<h1>
+				CONTACT US</h1></div>
+			</Container>
+			</Element>		
+<center className="border">			
+<h1 >Location</h1>
+<h3>Nagercoil, Tamil Nadu, India <br />
+	Mobile No:123456789
+</h3>
+<iframe 
+src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126271.88635351662!2d76.8417645615645!3d8.499724288046535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b05bbb805bbcd47%3A0x15439fab5c5c81cb!2sThiruvananthapuram%2C%20Kerala!5e0!3m2!1sen!2sin!4v1711869056543!5m2!1sen!2sin" width="1300" height="450"   loading="lazy" ></iframe> 
+
+<br /> <br />
+</center>
+		</div>
+		
+		</>
+
+
+	)
+	
+}
+
+export default contact
